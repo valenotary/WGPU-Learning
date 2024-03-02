0a677dce8d819632c09722f37905ae97d01baede
Tony Varela <valenotary@Tonys-MacBook-Pro.local>
2024-03-02 00:15:48 -0800

    surface tutorial

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,247 @@
-use log::info;
+use log::{debug, error};
 use winit::{
     event::{Event, WindowEvent, KeyEvent},
     event_loop::{ControlFlow, EventLoop},
     keyboard::{KeyCode, PhysicalKey},
-    window::WindowBuilder,
+    window::{Window, WindowBuilder},
 };
 
-pub fn run() {
+//TODO: refactor by pruning/renaming for more precise responsibility
+struct State<'window> {
+    surface: wgpu::Surface<'window>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    render_pipeline: wgpu::RenderPipeline,
+}
+
+impl<'window> State<'window> {
+    // Creating some of the wgpu types requires async code
+    async fn new(window: &'window Window) -> Self {
+        let size = window.inner_size();
+
+        // creates adapters and surfaces
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            ..Default::default()
+        });
+
+        // the part of the window we draw to
+        let surface = instance.create_surface(window).unwrap();
+
+        // the adapter is the handle to the gpu
+        let adapter = instance.request_adapter(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            },
+        ).await.unwrap();
+
+        let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
+            label: None,
+            required_features: wgpu::Features::empty(),
+            required_limits: wgpu::Limits::default(),
+        }, None).await.unwrap();
+
+        let capabilities = surface.get_capabilities(&adapter);
+
+        // Shader code in this tutorial assumes an sRGB surface texture. Using a different
+        // one will result in all the colors coming out darker. If you want to support non
+        // sRGB surfaces, you'll need to account for that when drawing to the frame.
+        let format = capabilities.formats.iter()
+            .copied().find(|f| f.is_srgb())
+            .unwrap_or(capabilities.formats[0]);
+
+        // this will define how the surface will create its underlying
+        // SurfaceTextures
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format,
+            width: size.width,
+            height: size.height,
+            present_mode: capabilities.present_modes[0],
+            desired_maximum_frame_latency: 0,
+            alpha_mode: capabilities.alpha_modes[0],
+            view_formats: vec![],
+        };
+        surface.configure(&device, &config);
+
+        let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+        let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("Render Pipeline Layout"),
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+        });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main",
+                buffers: &[],
+            },
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList, // every three verties correspond to one triangle
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                unclipped_depth: false,
+                polygon_mode: wgpu::PolygonMode::Fill,
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            multiview: None,
+        });
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            render_pipeline
+        }
+    }
+
+    // pub fn window(&self) -> &Window {
+    //     &self.window
+    // }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn input(&mut self, _event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {
+        // todo!()
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        // wait for the surface to provide a new SurfaceTexture to render to
+        let output = self.surface.get_current_texture()?;
+        // create a texture view with default settings; required for how the render code interacts
+        // with the texture
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+        // the encoder builds up a command buffer before being submitted to the GPU
+        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+        // why do we have an extra {} block around _render_pass?
+        // begin_render_pass borrows encoder mutably (&mut self)
+        // and we cant call encoder.finish() until we release the borrow
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment { // This is what @location(0) in the fragment shader targets
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+            });
+
+            // different pipelines can get coniditionally set in set_pipeline
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
+        }
+
+        // finish command buffer, submit to renderer
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
+    }
+}
+
+pub async fn run() {
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let mut state = State::new(&window).await;
 
     // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
     // dispatched any events. This is ideal for games and similar applications.
     event_loop.set_control_flow(ControlFlow::Poll);
 
-    event_loop.run(move |event, elwt| {
+    event_loop.run(|event, elwt| {
         match event {
-            // curious: is it idiomatic to have these different WindowEvents separated by a |,
-            // or should we implement an inner match?
             Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } | Event::WindowEvent {
-                event: WindowEvent::KeyboardInput {
-                    event: KeyEvent {
-                        physical_key: PhysicalKey::Code(KeyCode::Escape),
+                ref event,
+                window_id
+            } if window_id == window.id() => if !state.input(event) {
+                match event {
+                    WindowEvent::CloseRequested | WindowEvent::KeyboardInput {
+                        event: KeyEvent {
+                            physical_key: PhysicalKey::Code(KeyCode::Escape),
+                            ..
+                        },
                         ..
-                    },
-                    ..
-                },
-                ..
-            } => {
-                info!("The close button was pressed; stopping");
-                elwt.exit();
+                    } => {
+                        debug!("The close button was pressed; stopping");
+                        elwt.exit();
+                    }
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                    }
+                    // WindowEvent::ScaleFactorChanged { .. } => {
+                    //
+                    // }
+                    WindowEvent::RedrawRequested => {
+                        // Redraw the application.
+                        //
+                        // It's preferable for applications that do not render continuously to render in
+                        // this event rather than in AboutToWait, since rendering in here allows
+                        // the program to gracefully handle redraws requested by the OS.
+
+                        // TODO: I think I just need to redraw in AboutToWait instead of here, right?
+                        state.update();
+                        match state.render() {
+                            Ok(_) => {}
+                            // Reconfigure the surface if lost
+                            Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                            // The system is out of memory, we should probably quit
+                            Err(wgpu::SurfaceError::OutOfMemory) => elwt.exit(),
+                            // All other errors (Outdated, Timeout) should be resolved by the next frame
+                            Err(e) => error!("{:?}", e),
+                        }
+                    }
+                    _ => {}
+                }
             }
             Event::AboutToWait => {
                 // Application update code.
@@ -43,16 +252,6 @@ pub fn run() {
                 // applications which do not always need to. Applications that redraw continuously
                 // can render here instead.
                 window.request_redraw();
-            }
-            Event::WindowEvent {
-                event: WindowEvent::RedrawRequested,
-                ..
-            } => {
-                // Redraw the application.
-                //
-                // It's preferable for applications that do not render continuously to render in
-                // this event rather than in AboutToWait, since rendering in here allows
-                // the program to gracefully handle redraws requested by the OS.
             }
             _ => ()
         }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use wgpu_learning::run;
 
 fn main() {
- run();
+ pollster::block_on(run());
 }
